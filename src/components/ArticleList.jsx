82a54a1aa3cb93871b631f0d277d10effc87ab2a
Scott <scottobrien93@outlook.com>
2024-11-20 20:41:32 +0000

    user can view a list of all articles

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -9,10 +9,41 @@ function ArticleList() {
   useEffect(() => {
     setLoading(true);
     setError(false);
-    fetchArticles();
+    fetchArticles()
+      .then((articles) => {
+        setArticles(articles);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+        setError(true);
+      });
   }, []);
 
-  return <section></section>;
+  if (error) {
+    return <p> There has been an error!!!</p>;
+  }
+
+  if (loading) {
+    return <p>Loading articles...</p>;
+  }
+
+  if (articles.length === 0) {
+    return <p>No articles available.</p>;
+  }
+
+  return (
+    <section>
+      {articles.map((article) => {
+        return (
+          <div key={article.article_id}>
+            <h3>{article.title}</h3>
+            <p>{article.body}</p>
+          </div>
+        );
+      })}
+    </section>
+  );
 }
 
 export default ArticleList;
